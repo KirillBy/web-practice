8d66c35cd7a9d243a372731dda19dde0de0f275b
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-06 23:30:45 +0300

    060720 1 task done

---
+++ HW06072020-1/HW06072020-1/Controllers/CustumersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW06072020_1.Controllers
+{
+    public class CustumersController : Controller
+    {
+        // GET: Custumers
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ HW06072020-1/HW06072020-1/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW06072020_1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        // GET: Product
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
